2c1f958a72a75f3122f92163a208eb68b9875ab1
Vuthy_Tourn <vuthytuon168@gmail.com>
2026-04-17 15:11:25 +0700

    First Commit

---
+++ src/test/java/com/piseth/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piseth.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
